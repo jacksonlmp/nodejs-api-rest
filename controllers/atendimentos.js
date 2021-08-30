dfb857f04c84b368067e7a3ebcd600571150aee1
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-08-30 15:02:09 -0300

    Modules

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rodando All in Antedimentos'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Rota de Atendimento e POST')})
+
 }
 

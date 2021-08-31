bdb45bf73c0920cf3f34d5e696cdb97c47e15327
jacksonlmp <jacksonlmp17.jl@gmail.com>
2021-08-31 16:08:56 -0300

    Excessoes

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,6 +6,22 @@ class Atendimento{
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cleinte.lenght >=5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
         
         const sql = 'INSERT INTO Atendimentos SET ?'
 
